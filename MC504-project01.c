a32cda0af99c1b0a503cbef541cda9c82faff294
cmillani <carloseduardomillani@gmail.com>
2015-04-09 12:13:30 -0300

    Ideias. Tentativa de coordenar passagem

--- MC504-project01.c
+++ MC504-project01.c
@@ -23,10 +23,13 @@ bat_queue north, south, west, east;
 
 //pthread_t n_queue[40], w_queue[40], s_queue4[40], e_queue[40]; //One set of threads for each of the sides of the cross -- talvez melhor implementar uma lista ligada? just call (saul) next
 //lista ligada elimina necessidade do side_now :v e o codigo nao tem limite de carrinhows \o/ codigo monxtro
-pthread_mutex_t n_insert_mutex = PTHREAD_MUTEX_INITIALIZER, s_insert_mutex = PTHREAD_MUTEX_INITIALIZER
-, w_insert_mutex = PTHREAD_MUTEX_INITIALIZER, e_insert_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t n_wait = PTHREAD_COND_INITIALIZER, s_wait = PTHREAD_COND_INITIALIZER
-, w_wait = PTHREAD_COND_INITIALIZER, e_wait = PTHREAD_COND_INITIALIZER;
+pthread_t batman_thread;
+pthread_mutex_t n_insert_mutex = PTHREAD_MUTEX_INITIALIZER, s_insert_mutex = PTHREAD_MUTEX_INITIALIZER, w_insert_mutex = PTHREAD_MUTEX_INITIALIZER, e_insert_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t n_remove_mutex = PTHREAD_MUTEX_INITIALIZER, s_remove_mutex = PTHREAD_MUTEX_INITIALIZER, w_remove_mutex = PTHREAD_MUTEX_INITIALIZER, e_remove_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t n_wait_mutex = PTHREAD_MUTEX_INITIALIZER, s_wait_mutex = PTHREAD_MUTEX_INITIALIZER, w_wait_mutex = PTHREAD_MUTEX_INITIALIZER, e_wait_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t batman_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t n_wait_first = PTHREAD_COND_INITIALIZER, s_wait_first = PTHREAD_COND_INITIALIZER, w_wait_first = PTHREAD_COND_INITIALIZER, e_wait_first = PTHREAD_COND_INITIALIZER;
+pthread_cond_t batman_help_me = PTHREAD_COND_INITIALIZER;
 
 void *north_arrive(void *tid)
 {
@@ -36,43 +39,132 @@ void *north_arrive(void *tid)
 	n_size++;//Se deixar vetor tem que checar o maximo!
 	//insere na fila
 	pthread_mutex_unlock(&n_insert_mutex);
-	//aguarda ser chamado
+	
+	pthread_mutex_lock(&n_remove_mutex);
+	if (pthread_equal(pthread_self(), north.next->node))//Primeiro dessa fila? se sim ve se pode ir e notifica proximo
+	{
+		if (west.next == NULL)//verifica direita
+		{
+			printf("First at north\n");
+		}
+		else//Tem na direita, espera sinal e avisa batman que ta esperando;
+		{
+			pthread_mutex_lock(&batman_lock);
+			pthread_cond_signal(&batman_help_me);
+			pthread_mutex_lock(&batman_lock);
+			printf("Better call BATMAN!\n");
+		}//lembrar caso que se a fila ficar vazia nunca mais volta.....
+		//chama o prox da fila
+		pthread_mutex_lock(&n_wait_mutex);
+		pthread_cond_signal(&n_wait_first);
+		pthread_mutex_unlock(&n_wait_mutex);
+	}
+	else//Aguarda ser chamado pelo da frente
+	{
+		pthread_mutex_lock(&n_wait_mutex);
+		pthread_cond_wait(&n_wait_first, &n_wait_mutex);
+		printf("Called next north\n");
+	}
+	pthread_mutex_unlock(&n_remove_mutex);
 }
 void *south_arrive(void *tid)
 {
 	int* my_number = (int*)tid;
 	printf("BAT %d S chegou no cruzamento\n", *my_number);
-	pthread_mutex_lock(&n_insert_mutex);
+	pthread_mutex_lock(&s_insert_mutex);
 	s_size++;
 	//insere na fila
-	pthread_mutex_unlock(&n_insert_mutex);
-	//aguarda ser chamado
+	pthread_mutex_unlock(&s_insert_mutex);
+
+	pthread_mutex_lock(&s_remove_mutex);
+	if (pthread_equal(pthread_self(), south.next->node))//Primeiro dessa fila? se sim ve se pode ir e notifica proximo
+	{
+		if (east.next == NULL)//verifica direita
+		{
+			printf("First at south\n");
+		}
+		else//Tem na direita, espera sinal e avisa bat que ta esperando;
+		{
+			
+		}
+		//chama o prox da fila
+	}
+	else//Aguarda ser chamado pelo da frente
+	{
+		printf("sec at south\n");
+	}
+	pthread_mutex_unlock(&s_remove_mutex);
 }
 void *east_arrive(void *tid)
 {
 	int* my_number = (int*)tid;
 	printf("BAT %d E chegou no cruzamento\n", *my_number);
-	pthread_mutex_lock(&n_insert_mutex);
+	pthread_mutex_lock(&e_insert_mutex);
 	e_size++;
 	//insere na fila
-	pthread_mutex_unlock(&n_insert_mutex);
-	//aguarda ser chamado
+	pthread_mutex_unlock(&e_insert_mutex);
+	
+	pthread_mutex_lock(&e_remove_mutex);
+	if (pthread_equal(pthread_self(), east.next->node))//Primeiro dessa fila? se sim ve se pode ir e notifica proximo
+	{
+		if (north.next == NULL)//verifica direita
+		{
+			printf("First at East\n");
+		}
+		else//Tem na direita, espera sinal e avisa bat que ta esperando;
+		{
+			
+		}
+		//chama o prox da fila
+	}
+	else//Aguarda ser chamado pelo da frente
+	{
+		printf("sec at east\n");
+	}
+	pthread_mutex_unlock(&e_remove_mutex);
 }
 void *west_arrive(void *tid)
 {
 	int* my_number = (int*)tid;
 	printf("BAT %d W chegou no cruzamento\n", *my_number);
-	pthread_mutex_lock(&n_insert_mutex);
+	pthread_mutex_lock(&w_insert_mutex);
 	w_size++;
 	//insere na fila
-	pthread_mutex_unlock(&n_insert_mutex);
-	//aguarda ser chamado
+	pthread_mutex_unlock(&w_insert_mutex);
+	
+	pthread_mutex_lock(&w_remove_mutex);
+	if (pthread_equal(pthread_self(), west.next->node))//Primeiro dessa fila? se sim ve se pode ir e notifica proximo
+	{
+		if (south.next == NULL)//verifica direita
+		{
+			printf("First at west\n");
+		}
+		else//Tem na direita, espera sinal e avisa bat que ta esperando;
+		{
+			
+		}
+		//chama o prox da fila
+	}
+	else//Aguarda ser chamado pelo da frente
+	{
+		printf("sec at west\n");
+	}
+	pthread_mutex_unlock(&w_remove_mutex);
 }
 
 void *batman(void* tid)
 {
-	
+	int block_count = 0;
+	while (block_count < 4)
+	{
+		pthread_mutex_lock(&batman_lock);
+		printf("Wait\n");
+		pthread_cond_wait(&batman_help_me, &batman_lock);
+		block_count++;
+	}
 }
+//contador de giro--ruim, quando batman deve agir, deve ser fila?
+//Posso usar um semaforo ou tem que ser cruzamento mesmo? o batman deve gerenciar tudo ou os proprios bats devem ir andando ate dar condicao de travamento?
 
 int main(int argc, char* argv[])
 {
@@ -80,12 +172,13 @@ int main(int argc, char* argv[])
 	north.next = NULL;
 	if (argc > 1)
 	{
+		pthread_create(&batman_thread, NULL, batman, 0);
 		int i = 0;
 		while (argv[1][i] != '\0')
 		{
 			int *i_copy = malloc(sizeof(int));
 			*i_copy = i + 1;
-			switch (argv[1][i]) //newsnsnnns
+			switch (argv[1][i]) //newsensnennse
 			{
 				case 'e': {
 					e_end->next = malloc(sizeof(bat_queue));
@@ -111,7 +204,7 @@ int main(int argc, char* argv[])
 					pthread_create(&(s_end->node), NULL, south_arrive,(void*)(i_copy));
 					break;
 				}
-				default :{
+				default : {
 					exit(EXIT_FAILURE);
 				}
 			}
@@ -121,7 +214,6 @@ int main(int argc, char* argv[])
 			i++;
 		}
 	}
-	int* i;
 	setbuf(stdin, 0);
 	getchar();
 	printf("w%d e%d s%d n%d\n",w_size, e_size, s_size, n_size);
